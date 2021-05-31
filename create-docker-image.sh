8a553a27d743a690b0107d319cf61c200a0c2460
i1uxaermakov <ilya.ermakov.acc@gmail.com>
2021-05-31 15:42:03 -0500

    Create a script to build a jar, build an image, and push the image to the repository

---
+++ create-docker-image.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+# Color for output
+blue=$(tput setaf 4)
+bold=$(tput bold)
+boldBlue=${bold}${blue}
+reset=$(tput sgr0)
+
+echo "\n\n${boldBlue}STARTING THE SCRIPT"
+echo "${boldBlue}Building a jar of the project"
+mvn package -Dmaven.test.skip=true
+
+echo -e "\n\n${boldBlue}Building a Docker image based on the local DockerFile and the jar"
+docker image build --tag ilyaermakov/survivor-fitness-backend:latest .
+
+echo -e "\n\n${boldBlue}Pushing the image to the repository${reset}"
+docker push ilyaermakov/survivor-fitness-backend:latest
+
+
