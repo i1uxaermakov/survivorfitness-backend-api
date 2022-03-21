904bcff6c90a98c99b3601f9516b7368e097f3a8
i1uxaermakov <ilya.ermakov.acc@gmail.com>
2022-03-20 20:55:49 -0500

    Add change-password functionality

--- create-docker-image.sh
+++ create-docker-image.sh
@@ -5,7 +5,7 @@ bold=$(tput bold)
 boldBlue=${bold}${blue}
 reset=$(tput sgr0)
 
-imageTag="v9-add-users-email"
+imageTag="v10-location-assignments"
 
 echo "\n\n${boldBlue}STARTING THE SCRIPT"
 echo "${boldBlue}Building a jar of the project"
---
+++ src/main/java/com/changeplusplus/survivorfitness/backendapi/dto/ChangePasswordRequest.java
@@ -0,0 +1,15 @@
+package com.changeplusplus.survivorfitness.backendapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequest {
+    private String currentPassword;
+    private String newPassword;
+}
